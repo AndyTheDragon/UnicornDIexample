d88cffc578d2e8c41e732c61e9d6322d039f0196
AndyTheDragon <andre.jansam@gmail.com>
2025-04-12 11:28:54 +0200

    First try at implementation of databaseDAO

--- src/main/java/dat/dao/DatabaseDAO.java
+++ src/main/java/dat/dao/DatabaseDAO.java
@@ -4,6 +4,8 @@ import dat.config.ConnectionPool;
 import dat.entities.Unicorn;
 import dat.exceptions.DaoException;
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,6 +17,7 @@ import java.util.Map;
 public class DatabaseDAO implements ICrudDAO<Unicorn>
 {
     private final ConnectionPool connectionPool = ConnectionPool.getInstance();
+    private final Logger logger = LoggerFactory.getLogger(DatabaseDAO.class);
 
     public DatabaseDAO() {
         // Default constructor
@@ -40,12 +43,14 @@ public class DatabaseDAO implements ICrudDAO<Unicorn>
             }
             else
             {
+                logger.error("Failed to create entity, no ID obtained.");
                 throw new DaoException("Failed to create entity, no ID obtained.");
             }
             return entity;
         }
         catch (SQLException e)
         {
+            logger.error("Error creating entity. ", e);
             throw new DaoException("Error creating entity", e);
         }
     }
@@ -72,30 +77,82 @@ public class DatabaseDAO implements ICrudDAO<Unicorn>
             }
             else
             {
+                logger.error("Entity with ID {} not found", id);
                 throw new DaoException("Error fetching entity by ID: Entity with this ID does not exist");
             }
         }
         catch (SQLException e)
         {
+            logger.error("Error fetching entity by ID. ", e);
             throw new DaoException("Error fetching entity by ID", e);
         }
     }
 
     @Override
-    public List<Unicorn> getAll()
+    public List<Unicorn> getAll() throws DaoException
     {
-        throw new UnsupportedOperationException("Not implemented yet");
+        String sql = "SELECT * FROM unicorns";
+        try (Connection db = connectionPool.getConnection();
+             PreparedStatement ps = db.prepareStatement(sql))
+        {
+            var rs = ps.executeQuery();
+            List<Unicorn> unicorns = new java.util.ArrayList<>();
+            while (rs.next())
+            {
+                Unicorn unicorn = new Unicorn(
+                        rs.getString("unicorn_name"),
+                        rs.getInt("age"),
+                        rs.getString("color"),
+                        rs.getDouble("powerstrength")
+                );
+                unicorn.setId(rs.getInt("id"));
+                unicorns.add(unicorn);
+            }
+            return unicorns;
+        }
+        catch (SQLException e)
+        {
+            logger.error("Error fetching all entities. ", e);
+            throw new DaoException("Error fetching all entities", e);
+        }
     }
 
     @Override
     public Unicorn update(Unicorn entity) throws DaoException
     {
-        throw new UnsupportedOperationException("Not implemented yet");
+        String sql = "UPDATE unicorns SET unicorn_name = ?, age = ?, color = ?, powerstrength = ? WHERE id = ?";
+        try (Connection db = connectionPool.getConnection();
+             PreparedStatement ps = db.prepareStatement(sql))
+        {
+            ps.setString(1, entity.getName());
+            ps.setInt(2, entity.getAge());
+            ps.setString(3, entity.getColor());
+            ps.setDouble(4, entity.getPowerStrength());
+            ps.setInt(5, entity.getId());
+            ps.executeUpdate();
+            return entity;
+        }
+        catch (SQLException e)
+        {
+            logger.error("Error updating entity. ", e);
+            throw new DaoException("Error updating entity", e);
+        }
     }
 
     @Override
     public void delete(Unicorn entity) throws DaoException
     {
-        throw new UnsupportedOperationException("Not implemented yet");
+        String sql = "DELETE FROM unicorns WHERE id = ?";
+        try (Connection db = connectionPool.getConnection();
+             PreparedStatement ps = db.prepareStatement(sql))
+        {
+            ps.setInt(1, entity.getId());
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            logger.error("Error deleting entity. ", e);
+            throw new DaoException("Error deleting entity", e);
+        }
     }
 }
